a2c8866a825c5c9b9829f2c5106b6c75e7a09c88
mexcampos <camposmario1996@gmail.com>
2023-03-27 21:11:44 -0300

    cambios en honorarios de abogado y npm para num to letras

--- aclrtavc.js
+++ aclrtavc.js
@@ -1,85 +1,79 @@
 // funcion switch para elegir vocal
-function elegirVariable(variable1, variable2, variable3, variable4) {
-  let opcion = prompt(`Por favor, elige una opción:\n1. ${variable1}\n2. ${variable2}\n3. ${variable3}\n`);
+function elegirVocal() {
+  let opcion = prompt(`Por favor, elige una vocal:\n1. Cristián Requena\n2. María Cecilia Busleiman\n3. Sofía Andrea Keselman\n`);
   switch (opcion) {
-  case "1":
-    return variable1;
-  case "2":
-    return variable2;
-  case "3":
-    return variable3;
-  default:
-    return "Opción inválida";
+    case "1":
+      return "Cristián Requena";
+    case "2":
+      return "María Cecilia Busleiman";
+    case "3":
+      return "Sofía Andrea Keselman";
+    default:
+      return "Opción inválida";
   }
 }
+
 //continuacion de funcion elegir vocal
-// uso: miVariable es la que hay que llamar en el HTML  
-  let miVariable = elegirVariable("Cristián Requena", "María Cecilia Busleiman", "Sofía Andrea Keselman");
-  console.log(miVariable);   
-
-//obtencion de la variable que el usuario eligió dentro de la función mi variable
-  console.log("La opción elegida fue: " + miVariable);
-  prompt("La opción elegida fue: " + miVariable);
-  console.log(miVariable);
-
-// ahora miVariable vale la opcion que eligio el usuario
+let vocalElegida = elegirVocal();
+console.log("La vocal elegida fue: " + vocalElegida);
+alert("La vocal elegida fue: " + vocalElegida);
     
 // Preguntas para el usuario retomadas
-  fecha = prompt("Fecha (dd/mm/aaaa):");
-  autos = prompt("Carátula de los autos (sin comillas):");
-  expteNum = prompt("Número de expediente:");
-  actor = prompt("Nombre del actor/a");
-  dni = prompt("DNI del actor/a:");
-  abActor = prompt("Abogado del actor:");
-  demandada = prompt("Demandada:");
-  abDemandada = prompt("Abogado de la demandada:");
-  acuerdo = prompt("Acuerdo");
-  baseAcuerdo = prompt("Monto del Acuerdo: ");
-  perito = prompt("Perito Médico:");
-  jus = prompt("Regulación perito (jus): ");
-  honAbActor = prompt("Honorarios del abogado del actor (el decimal con puntos): ");
+fecha = prompt("Fecha (dd/mm/aaaa):");
+autos = prompt("Carátula de los autos (sin comillas):");
+expteNum = prompt("Número de expediente:");
+actor = prompt("Nombre del actor/a");
+dni = prompt("DNI del actor/a:");
+abActor = prompt("Abogado del actor:");
+demandada = prompt("Demandada:");
+abDemandada = prompt("Abogado de la demandada:");
+acuerdo = prompt("Acuerdo");
+baseAcuerdo = prompt("Monto del Acuerdo: ");
+perito = prompt("Perito Médico:");
+jus = prompt("Regulación perito (jus): ");
+//import numberToSpanishWords from 'number-to-spanish-words';
 
-
-  
 // funcion para determinar mas de una vez los id en el texto
-  let actorUno = actor;
-  let dniUno = dni;  
-  let abActorUno = abActor;
-  let abActorDos = abActor;
-  let abDemandadaUno = abDemandada;
-  let abDemandadaDos = abDemandada;
-  let demandadaUno = demandada;
-  let demandadaDos = demandada;
-  let demandadaTres = demandada;
-  
+let actorUno = actor;
+let dniUno = dni;  
+let abActorUno = abActor;
+let abActorDos = abActor;
+let abDemandadaUno = abDemandada;
+let abDemandadaDos = abDemandada;
+let demandadaUno = demandada;
+let demandadaDos = demandada;
+let demandadaTres = demandada;
   
 // funcion para calcular los honorarios del perito médico
-jusNum = parseInt(jus) * 5968.91;
-console.log(jusNum);
-var result = "$ " + jusNum.toLocaleString('de-DE', {minimumFractionDigits: 2,maximumFractionDigits: 2});
-console.log(result);
+const valorJus = 5968.91;
+var honPerMedNum = parseInt(jus) * valorJus;
+console.log(honPerMedNum);
+var honPerMedNumResult = "$ " + honPerMedNum.toLocaleString('de-DE', {minimumFractionDigits: 2,maximumFractionDigits: 2});
+console.log(honPerMedNumResult);
   
 // aportes
-numAportes = jusNum * 0.15;
-let aportes = numAportes.toFixed(2);
-console.log(aportes);
+numAportes = honPerMedNum * 0.15;
+console.log(numAportes);
 var resultAportes = "$ " + numAportes.toLocaleString('de-DE', {minimumFractionDigits: 2, maximumFractionDigits: 2});
 console.log(resultAportes);
 
 // IVA perito
-numIvaPerito = jusNum * 0.21;
-let ivaPerito = numIvaPerito.toFixed(2);
-console.log(aportes);
+numIvaPerito = honPerMedNum * 0.21;
+console.log(numIvaPerito);
 var resultIvaPerito = "$ " + numIvaPerito.toLocaleString('de-DE', {minimumFractionDigits: 2, maximumFractionDigits: 2});
 console.log(resultIvaPerito);
 
-// Honorarios del abogado del actor
-honAbActorNum = parseInt(honAbActor) * 1.00;
-console.log(honAbActorNum);
+// Habria que hacer una funcion if-else para elegir si hay que regular los honorarios del abogado o si están previamente estipulados
+const regAb = prompt("¿Se debe regular honorarios al letrado del actor? si/no");
+if (regAb === "si") {
+    const porcRegAb = prompt("En qué porcentaje? Usar decimales con punto");
+    var honAbActorNum = baseAcuerdo * parseFloat(porcRegAb) / 100;
+} else {
+    var honAbActorNumNo = prompt("Monto de honorarios acordados: ");
+    honAbActorNum = honAbActorNumNo * 1;
+}
 var honAbActorResult = "$ " + honAbActorNum.toLocaleString('de-DE', {minimumFractionDigits: 2, maximumFractionDigits: 2});
 console.log(honAbActorResult);
-
-// Habria que hacer una funcion if-else para elegir si hay que regular los honorarios del abogado o si están previamente estipulados
 
 //Determinar la tasa de justicia
 var tasa = parseInt(baseAcuerdo) * 0.02;
@@ -92,6 +86,7 @@ console.log(tasaResult);
   var tasaResult = "$ " + tasaMinimo.toLocaleString('de-DE', {minimumFractionDigits: 2,maximumFractionDigits: 2});
   var tasaMinimoLetras = "ocho mil novecientos cincuenta y tres con cuarenta";
 }
+
 // Determinar los aportes colegiales
 var colegio = parseInt(baseAcuerdo) * 0.01;
 var colegioMinimo = 2280.00;
@@ -126,8 +121,9 @@ document.getElementById("acuerdo").textContent = acuerdo;
 document.getElementById("perito").textContent = perito;
 document.getElementById("jus").textContent = jus;
 document.getElementById("resultAportes").textContent = resultAportes;
-document.getElementById("miVariable").textContent = miVariable;
-document.getElementById("result").textContent = result;
+document.getElementById("vocalElegida").textContent = vocalElegida;
+document.getElementById("honPerMedNumResult").textContent = honPerMedNumResult;
+document.getElementById("honPerMedLet").textContent = honPerMedLet;
 document.getElementById("honAbActorResult").textContent = honAbActorResult;
 document.getElementById("resultIvaPerito").textContent = resultIvaPerito;
 document.getElementById("tasaResult").textContent = tasaResult;
--- test.js
+++ test.js
@@ -1,19 +1,4 @@
-// Define the sample text with blank spaces
-const sampleText = "I like to ________ and ________ on the weekends.";
-
-// Ask the user for input
-const userInput = prompt("Please enter a verb:");
-
-// Fill the blanks with the user's input
-const filledText = sampleText.replace("________", userInput);
-
-// Ask for more input for the second blank
-const userInput2 = prompt("Please enter another verb:");
-
-// Fill the second blank with the user's input
-const finalText = filledText.replace("________", userInput2);
-
-// Output the final text with the filled blanks
-console.log(finalText);
-
-  
+var jusNum = parseInt(jus) * 5968.91;
+console.log(jusNum);
+var result = "$ " + jusNum.toLocaleString('de-DE', {minimumFractionDigits: 2,maximumFractionDigits: 2});
+console.log(result);
